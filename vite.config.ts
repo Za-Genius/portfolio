08332b5da478471c62d996b63299b6ab0cf81a78
Za_Genius <rapzter58@gmail.com>
2025-02-25 10:55:24 +0100

    fix: adjust vite config
    set my base in my vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), TanStackRouterVite()],
-  base: "/portfolio",
+  base: "/",
 });
